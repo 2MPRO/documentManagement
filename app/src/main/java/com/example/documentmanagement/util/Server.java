49672f8e1faddc3ab9851bba33c1157b59fd4e6c
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2021-12-17 09:40:58 +0700

    ThanhNhanTaoUtil

---
+++ app/src/main/java/com/example/documentmanagement/util/Server.java
@@ -0,0 +1,6 @@
+package com.example.documentmanagement.util;
+
+public class Server {
+    public static String localhost="192.168.1.118";
+    public static String Đuongdandangnhap = "http://" + localhost + "TH_Android/LoginDocument/login.php" ;
+}
