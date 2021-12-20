051d71996e33461255a3722bdd4ab21be5a8c4d1
Thành Nhân <nguyenthanhnhan12701@gmail.com>
2021-12-20 22:46:27 +0700

    Thanh Nhan

--- app/src/main/java/com/example/documentmanagement/Controllers/Activity/AccountInformationActivity.java
+++ app/src/main/java/com/example/documentmanagement/Controllers/Activity/AccountInformationActivity.java
@@ -1,19 +1,31 @@
 package com.example.documentmanagement.Controllers.Activity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import com.example.documentmanagement.R;
 
 public class AccountInformationActivity extends AppCompatActivity {
-
-
+    EditText etHoTen, etSDT, etMail, etNgay, etDiaChi;
+    Button btnCapNhat;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account_information);
+
+
     }
+
+    private void mapping() {
+        etHoTen = findViewById(R.id.edt_tk_ht);
+        etSDT = findViewById(R.id.edt_tk_sdt);
+        etMail = findViewById(R.id.edt_tk_em);
+    }
+
 }
