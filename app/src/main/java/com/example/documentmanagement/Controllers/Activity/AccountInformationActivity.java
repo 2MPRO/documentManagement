3b06a359c2f8cb37f4b02e3f81ac59060bdd1439
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2021-12-20 22:43:57 +0700

    thien update

--- app/src/main/java/com/example/documentmanagement/Controllers/Activity/AccountInformationActivity.java
+++ app/src/main/java/com/example/documentmanagement/Controllers/Activity/AccountInformationActivity.java
@@ -1,5 +1,7 @@
 package com.example.documentmanagement.Controllers.Activity;
 
+import static com.example.documentmanagement.Controllers.Activity.LoginActivity.use;
+
 import android.os.Bundle;
 
 import androidx.annotation.Nullable;
@@ -14,6 +16,7 @@ public class AccountInformationActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        String uuu = use.get
         setContentView(R.layout.activity_account_information);
     }
 }
