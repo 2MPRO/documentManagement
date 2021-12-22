7614f905cebc528e4c9ce287d6f3d9b4309499f8
Phạm Văn Thiên <80672239+VanThienPham@users.noreply.github.com>
2021-12-22 11:12:08 +0700

    thien update

--- app/src/main/java/com/example/documentmanagement/Controllers/Activity/PermissionActivity.java
+++ app/src/main/java/com/example/documentmanagement/Controllers/Activity/PermissionActivity.java
@@ -4,17 +4,24 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ExpandableListView;
-import android.widget.ListView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.example.documentmanagement.R;
+import com.example.documentmanagement.model.Permission;
+import com.example.documentmanagement.model.Room;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PermissionActivity extends AppCompatActivity {
     ExpandableListView ExpandableListView_permission;
-    ListView lvVanThu_Per, lvQuanTri_Per;
+    private ArrayList<Room> roomArrayList;
+    private Map<Room, List<Permission>> listPermissions;
     Button btnLuu;
     Toolbar toolbar;
     @Override
@@ -22,10 +29,24 @@ public class PermissionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_permission);
         mapping();
+        setRoomArrayList();
         setToolbar();
         setButtonSave();
 
     }
+    private void setRoomArrayList(){
+        roomArrayList = new ArrayList<>();
+
+    }
+    private Map<Room, List<Permission>> getListPermissions(){
+        Map<Room, List<Permission>> listPermission = new HashMap<>();
+        Room room = new Room();
+        return  listPermissions;
+    }
+    private void  setListView(){
+
+    }
+
     private void setToolbar() {
         toolbar.setTitle("Phân quyền");
         toolbar.setTitleTextColor(this.getResources().getColor(R.color.white));
--- app/src/main/java/com/example/documentmanagement/Controllers/Adapter/ExpandableListviewAdapter.java
+++ app/src/main/java/com/example/documentmanagement/Controllers/Adapter/ExpandableListviewAdapter.java
@@ -4,6 +4,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseExpandableListAdapter;
+import android.widget.CheckBox;
 import android.widget.TextView;
 
 import com.example.documentmanagement.R;
@@ -82,7 +83,14 @@ public class ExpandableListviewAdapter extends BaseExpandableListAdapter {
 
     @Override
     public View getChildView(int groupPosition, int childPosition, boolean isLastChild, View convertView, ViewGroup parent) {
-        return null;
+        if(convertView == null){
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_rule,parent, false);
+            CheckBox checkBoxRule = convertView.findViewById(R.id.checkbox_rule);
+            Permission permission = listPermissions.get(romArrayList.get(groupPosition)).get(childPosition);
+            checkBoxRule.setText(permission.getPermissionName());
+
+        }
+        return convertView;
     }
 
     @Override
